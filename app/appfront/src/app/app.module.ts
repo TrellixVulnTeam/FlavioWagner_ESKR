19efc474cba0b6cb8a1fb0a08dfb8b0a3ea0d21b
Flávio Wagner <flavio-wagner@outlook.com>
2022-05-03 16:13:01 -0300

    Add front

--- app/appfront/src/app/app.module.ts
+++ app/appfront/src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ErrorsComponent } from './layouts/errors/errors.component';
+import { MainComponent } from './layouts/main/main.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorsComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
