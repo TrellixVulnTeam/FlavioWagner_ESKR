0d334060ef04503733bfc0e31e1afd17652685f2
Flavio Wagner <flavio-wagner@outlook.com>
2022-05-03 23:12:31 -0300

    Atualizar

--- app/appnovo/src/main/java/br/com/appnovo/controller/EstadoController.java
+++ app/appnovo/src/main/java/br/com/appnovo/controller/EstadoController.java
@@ -2,11 +2,12 @@ package br.com.appnovo.controller;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.appnovo.Enums.Genero;
 import br.com.appnovo.dto.EstadoDTO;
 import br.com.appnovo.service.EstadoService;
 
@@ -24,5 +25,10 @@ public class EstadoController  extends CustomClassController<EstadoService, Esta
 	protected void LoadData() {
 		setService(this.estadoService);
 		setMapper(this.mapper);
+	}
+	
+	@RequestMapping("/sigla/{uf}")
+	public ResponseEntity<EstadoDTO> Item(@PathVariable("uf") String uf){
+		return ResponseEntity.ok(estadoService.Item(uf));
 	}
 }
